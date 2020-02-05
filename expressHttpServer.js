31a2f8f0a9f13833358246870f2d84185c98f39b
devAon <yewon9742@gmail.com>
2020-02-05 13:30:52 +0900

    signup - token발급 및 JsonParse하여 부모페이지에 데이터 입력

--- expressHttpServer.js
+++ expressHttpServer.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var request = require('request');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -8,36 +9,34 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-app.get('/', function (req, res) {
-    res.render('test')
+app.get('/signup', function (req, res) {
+    res.render('signup');
 })
 
+app.get('/authResult', function (req, res) {
+    var authCode = req.query.code;
+    console.log(authCode);
 
+    var option = {
+        method: "POST",
+        url: "https://testapi.openbanking.or.kr/oauth/2.0/token",
+        headers : {
+            'Content-Type' : "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        form: {
+            code: authCode,
+            client_id: 'q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF',
+            client_secret: 'yVT6irMr2h4ZTHzZY7sDpbvhm1nlOzr4nP7DYRVy',
+            redirect_uri: 'http://localhost:3000/authResult',
+            grant_type: 'authorization_code'
+        }
+    }
 
-app.get('/designTest', function (req, res) {
-    res.render('designTest')
+    request(option, function (error, response, body) {
+        var parseData = JSON.parse(body);
+        res.render('resultChild', {data : parseData});
+    });
 })
 
-app.get('/wallet', function (req, res) {
-    res.render('wallet')
-})
-
-app.get('/home', function (req, res) {
-    res.send('home page')
-})
-
-app.get('/member', function (req, res) {
-    res.send('member page');
-})
-
-app.get('/join', function (req, res) {
-    res.json(1);
-    
-})
-
-app.post('/join', function (req, res) {
-    console.log(req.body)
-})
 
 app.listen(3000)
